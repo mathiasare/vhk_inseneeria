977e5ce3367eef9df4993a0735177a47edf0a56f
Mathias Are <are.mathias@gmail.com>
2026-03-13 16:03:31 +0200

    latest 1

--- 2026/device/VhkInseneeria2026_ArduinoIDE/secrets.h
+++ 2026/device/VhkInseneeria2026_ArduinoIDE/secrets.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define WIFI_SSID "iotest"
-#define WIFI_PASS "password"
+#define WIFI_SSID "" //Wifi nimi
+#define WIFI_PASS "" //Wifi parool
 
 #define WS_SERVER "inseback.mthsr.com"
 #define WS_PORT 443
-#define TEAM_ID 2
-#define WS_PATH "/ws/teams/2"
+#define TEAM_ID //Tiimi number nt. 1
+#define WS_PATH //"/ws/teams/1" viimane number on tiimi number
